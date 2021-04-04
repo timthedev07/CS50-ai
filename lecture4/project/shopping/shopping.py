d3697b2cd591dea145af403f070f705e06931559
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-04-04 12:20:36 +0200

    finished shopping project, but not yet graded

--- lecture4/project/shopping/shopping.py
+++ lecture4/project/shopping/shopping.py
@@ -31,6 +31,36 @@ def main():
     print(f"True Negative Rate: {100 * specificity:.2f}%")
 
 
+def monthAbbrToNum(month_input:str):
+    # turn it into lowercase letters
+    month = month_input.lower()
+    if month == "jan":
+        return 0
+    elif month == "feb":
+        return 1
+    elif month == "mar":
+        return 2
+    elif month == "apr":
+        return 3
+    elif month == "may":
+        return 4
+    elif month == "june":
+        return 5
+    elif month == "jul":
+        return 6
+    elif month == "aug":
+        return 7
+    elif month == "sep":
+        return 8
+    elif month == "oct":
+        return 9
+    elif month == "nov":
+        return 10
+    elif month == "dec":
+        return 11
+    else:
+        return None
+
 def load_data(filename):
     """
     Load shopping data from a CSV file `filename` and convert into a list of
@@ -59,15 +89,67 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    # open the file first
+    with open(filename, "r") as f:
 
+        # read the csv into the reader
+        reader = csv.reader(f)
+
+        # set up count, which is used to skip the header
+        count = 0
+
+        # set up a list for evidences
+        evidence = []
+
+        # set up a list for labels
+        labels = []
+
+        # iterate over the rows
+        for row in reader:
+            # if the current row is not the header
+            if count > 0:
+                buffer = []
+                for col in range(18):
+                    # else if column consists of integers
+                    if col in [0, 2, 4, 11, 12, 13, 14]:
+                        buffer.append(int(row[col]))
+                    # else if column consists of floating point numbers
+                    elif col in [1, 3, 5, 6, 7, 8, 9]:
+                        buffer.append(float(row[col]))
+                    # else if column is month
+                    elif col == 10:
+                        buffer.append(monthAbbrToNum(row[col]))
+                    # else if column is visitor type
+                    elif col == 15:
+                        # if user is a returning visitor
+                        if row[col] == "Returning_Visitor":
+                            buffer.append(1)
+                        else:
+                            buffer.append(0)
+                    # else if column is weekend
+                    elif col == 16:
+                        if row[col] == "TRUE":
+                            buffer.append(1)
+                        else:
+                            buffer.append(0)
+                    # finally if the column is revenue
+                    else:
+                        if row[col] == "TRUE":
+                            labels.append(1)
+                        else:
+                            labels.append(0)
+
+                evidence.append(buffer)
+            count += 1
+        return tuple([evidence, labels])
 
 def train_model(evidence, labels):
     """
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
+    return model.fit(evidence, labels)
 
 
 def evaluate(labels, predictions):
@@ -85,8 +167,25 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
-
+    sensitivity = 0
+    specificty = 0
+    positive_ind = []
+    negative_ind = []
+    for i in range(len(labels)):
+        if labels[i] == 0:
+            negative_ind.append(i)
+        else:
+            positive_ind.append(i)
+    # correct / total
+    for i in range(len(predictions)):
+        if i in negative_ind:
+            if predictions[i] == 0:
+                specificty += 1
+        elif i in positive_ind:
+            if predictions[i] == 1:
+                sensitivity += 1
+    return (sensitivity/len(positive_ind), specificty/len(negative_ind))
 
 if __name__ == "__main__":
     main()
+
